489967b61c709a9c7999466542e392dbf3bc4f7a
sydnenaal <dimasymbain@gmail.com>
2020-04-04 13:56:06 +0300

    add tabs body

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 function changeTab(tabNumber) {
   var percentsByTab = ["0%", "35%", "70%"];
 
+  var bodyPercents = ["0%", "-100%", "-200%"];
+
   var tabNumbers = ["1", "2", "3"];
 
   document.getElementById("line").style.marginLeft =
     percentsByTab[+tabNumber - 1];
 
-  tabNumbers.forEach(function(item) {
+  document.getElementById("tabs-body").style.marginLeft =
+    bodyPercents[+tabNumber - 1];
+
+  tabNumbers.forEach(function (item) {
     document.getElementById("tab-" + item).style.backgroundColor =
       tabNumber === item ? "#add136" : "#959595";
   });
